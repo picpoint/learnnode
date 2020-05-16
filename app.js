b16f6cf9b7d3b541864dc67800f3c4eeaac91474
picpoint <picpoint@yandex.ru>
2020-05-16 23:50:11 +0300

    learn book

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello Nodejs');
+const express = require('express');
+const app = express();
+const port = process.env.port || 4000;
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    title: 'My home page'
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`server start on port ${port}`);
+});
